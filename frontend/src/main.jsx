bc01840f1b34192d5a8c2f43e8ea2cff8c119ec5
mutahidaniel2000@gmail.com <mutahidaniel2000@gmail.com>
2025-11-18 18:36:03 +0300

    initial commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'  // ← Change this
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HashRouter>  {/* ← Change this */}
-      <App />
-    </HashRouter>
+    <App />  {/* ← No BrowserRouter here anymore */}
   </StrictMode>,
 )
