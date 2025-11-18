a954f18431c1fb58337dcfd644615ba777e507e0
mutahidaniel2000@gmail.com <mutahidaniel2000@gmail.com>
2025-11-18 14:21:57 +0300

    y

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'https://smart-waste-management-n9yk.onrender.com/api';
+const API_URL = 'https://smart-waste-management-n9yk.onrender.com/';
 // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
